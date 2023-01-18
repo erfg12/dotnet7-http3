2a6a57a7b007b6a324771c2f968541f573c3ce43
Jacob Fliss <newgmobile@gmail.com>
2023-01-18 09:14:57 -0500

    initial commit

---
+++ dotnet7-http3/Program.cs
@@ -0,0 +1,34 @@
+
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Net;
+
+namespace dotnet7_http3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.WebHost.ConfigureKestrel((context, options) =>
+            {
+                options.Listen(IPAddress.Any, 5001, listenOptions =>
+                {
+                    listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
+                    listenOptions.UseHttps();
+                });
+            });
+
+            var app = builder.Build();
+
+            app.MapGet("/", (HttpContext httpContext) =>
+            {
+                return $"{httpContext.Request.Protocol}";
+            });
+
+            app.Run();
+        }
+    }
+}
